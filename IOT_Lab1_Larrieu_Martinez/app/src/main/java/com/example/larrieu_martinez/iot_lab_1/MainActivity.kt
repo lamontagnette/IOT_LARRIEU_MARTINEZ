5f866149d4bdbb1e5bafdb16cb222f2b2fb480a1
lamontagnette <martinez.simon@outlook.fr>
2018-10-29 16:14:23 +0100

    OK beacons

--- IOT_Lab1_Larrieu_Martinez/app/src/main/java/com/example/larrieu_martinez/iot_lab_1/MainActivity.kt
+++ IOT_Lab1_Larrieu_Martinez/app/src/main/java/com/example/larrieu_martinez/iot_lab_1/MainActivity.kt
@@ -13,22 +13,47 @@ import com.android.volley.toolbox.DiskBasedCache
 import com.android.volley.toolbox.HurlStack
 import com.android.volley.toolbox.StringRequest
 
+import com.estimote.coresdk.common.requirements.SystemRequirementsChecker
+import com.estimote.coresdk.observation.region.beacon.BeaconRegion
+import com.estimote.coresdk.recognition.packets.Beacon
+import com.estimote.coresdk.service.BeaconManager
+import java.util.ArrayList
+import java.util.Collections
+import java.util.HashMap
+import java.util.UUID
+import android.widget.Toast
+
 class MainActivity : AppCompatActivity() {
 
+    private var beaconManager: BeaconManager? = null
+    private var region: BeaconRegion? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ////////////
-        //TODO : beacons modify room var. If nothing -> room = 0
-        var room = 1
-        ////////////
+        var room = 0
 
+        beaconManager = BeaconManager(this)
+        region = BeaconRegion("ranged region",
+                UUID.fromString("b9407f30-f5f8-466e-aff9-25556b57fe6d"), null, null)
 
-        server_request_devices(room)
+        beaconManager!!.setRangingListener(BeaconManager.BeaconRangingListener { region, list ->
+            if (!list.isEmpty()) {
+                val nearestBeacon = list[0]
+                val places = placesNearBeacon(nearestBeacon)
+                if (room != places!![0].toInt()){
+                    room = places!![0].toInt()
 
+                    server_request_devices(room)
+                }
+            }
+            else {
+                room = 0
+                server_request_devices(room)
+            }
+        })
     }
 
     fun server_request_devices(room : Int){
@@ -67,12 +92,54 @@ class MainActivity : AppCompatActivity() {
         }, object : Response.ErrorListener {
             override fun onErrorResponse(error: VolleyError) {
                 Toast.makeText(this@MainActivity, error.toString(), Toast.LENGTH_LONG).show()
-                Thread.sleep(500)
+                Thread.sleep(250)
                 server_request_devices(room)
             }
         })
 
         mRequestQueue.add(mStringRequest)
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        SystemRequirementsChecker.checkWithDefaultDialogs(this)
+
+        beaconManager!!.connect { beaconManager!!.startRanging(region) }
+    }
+
+    override fun onPause() {
+        beaconManager!!.stopRanging(region)
+        super.onPause()
+
+    }
+
+    private fun placesNearBeacon(beacon: Beacon): List<String>? {
+        val no_beacon_list = listOf("0")
+        val beaconKey = String.format("%d:%d", beacon.major, beacon.minor)
+        return if (PLACES_BY_BEACONS.containsKey(beaconKey)) {
+            PLACES_BY_BEACONS[beaconKey]
+        } else no_beacon_list
+    }
+
+    companion object {
+        private val PLACES_BY_BEACONS: Map<String, List<String>>
+
+        init {
+            val placesByBeacons = HashMap<String, List<String>>()
+            placesByBeacons["23899:517"] = object : ArrayList<String>() {
+                init {
+                    add("1") //beacon 3 -> room 1
+
+                }
+            }
+            placesByBeacons["23899:48851"] = object : ArrayList<String>() {
+                init {
+                    add("2") //beacon 13 -> room 2
+
+                }
+            }
+            PLACES_BY_BEACONS = Collections.unmodifiableMap(placesByBeacons)
+        }
     }
 }
